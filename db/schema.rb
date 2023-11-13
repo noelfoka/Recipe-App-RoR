fb18ac474231949b148cecb2ab7302a196371097
noelfoka <nomgnenoel@gmail.com>
2023-11-13 15:58:43 +0100

    generate recipe_foods model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_144840) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_145656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_144840) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+    t.index ["user_id"], name: "index_recipe_foods_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -42,5 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_144840) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipe_foods", "users"
   add_foreign_key "recipes", "users"
 end
