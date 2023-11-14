28b58eb1250478633c0717b268756895f4970f8b
noelfoka <nomgnenoel@gmail.com>
2023-11-14 16:13:39 +0100

    implemnted recipe

---
+++ db/migrate/20231114114819_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :preparation_time
+      t.string :cooking_time
+      t.text :description
+      t.boolean :public
+      t.integer :ingredient_counter
+      t.decimal :ingredient_value
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
