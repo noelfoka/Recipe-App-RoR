7bee438e16ebf0eb7e10a1ba74901d0b330d05ea
kcccendana <kc_telyn@yahoo.com>
2023-11-14 22:15:46 +0900

    Update foods_controller.rb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -25,7 +25,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: "Food was successfully created." }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class FoodsController < ApplicationController
   def update
     respond_to do |format|
       if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: "Food was successfully updated." }
+        format.html { redirect_to foods_url, notice: "Food was successfully updated." }
         format.json { render :show, status: :ok, location: @food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -65,6 +65,6 @@ class FoodsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def food_params
-      params.require(:food).permit(:name, :measurement_unit, :quantity, :user_id)
+      params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
     end
 end
