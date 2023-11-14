2fe2787f34ea15cd1aa36a481e1f9603f7e529ab
kcccendana <kc_telyn@yahoo.com>
2023-11-15 03:37:11 +0900

    add devise management

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+  devise_for :users
   resources :foods
   root 'home#index'
-  resources :foods, only: [:index] #do
-    # resources :foods, only: [:index, ]
-      # resources :comments, only: [:new, :create, :destroy]
-      # resources :likes, only: [:create] # Plural resources for handling likes
-    # end
-  #end
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
