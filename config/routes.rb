008c823d43e0852e759689b093c205c4b8a6aca6
noelfoka <nomgnenoel@gmail.com>
2023-11-15 14:29:32 +0100

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    member do
+      post 'toggle_public'
+    end
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy] 
+  end
   root 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_14_140012) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_084045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,5 +64,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_140012) do
   add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
   add_foreign_key "recipes_foods", "users"
-
 end
