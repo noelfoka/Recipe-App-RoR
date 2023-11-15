68cf5fe70925563e9967210c3768cf95c9069cd3
kcccendana <kc_telyn@yahoo.com>
2023-11-15 16:27:49 +0900

    fix linter error

--- Gemfile
+++ Gemfile
@@ -68,4 +68,4 @@ end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def index; end
+
   def destroy
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to root_url, notice: 'User was successfully destroyed.' }
     end
