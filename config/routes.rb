1265eb2db2857433ddb8659e486bc81ef313397d
kcccendana <kc_telyn@yahoo.com>
2023-11-16 15:24:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  resources :recipe_foods
   devise_for :users
   resources :foods
-  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
     member do
       post 'toggle_public'
     end
-    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy] 
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy, :edit, :update]
   end
+
   root 'home#index'
+
+  get 'general_shopping_list', to: 'home#shopping_list'
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
