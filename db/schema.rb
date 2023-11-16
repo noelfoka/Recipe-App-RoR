311fc9dd23dc2b4e95afd5bd136b79fb6764ba0d
kcccendana <kc_telyn@yahoo.com>
2023-11-16 15:24:32 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_084045) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_024704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_084045) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.string "preparation_time"
@@ -35,17 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_084045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
-  create_table "recipes_foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.float "price"
-    t.integer "quantity"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_recipes_foods_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_084045) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
-  add_foreign_key "recipes_foods", "users"
 end
